af44bdb029ccd2e2b476947ff53af107e77b3857
KaioBorgesDev <kaioeduardo401@outlook.com>
2024-07-05 15:48:22 -0300

    feat- profile beggin

--- components/cards/Profile.tsx
+++ components/cards/Profile.tsx
@@ -4,7 +4,14 @@ import React, { useEffect, useState } from 'react'
 import {View, Text, StyleSheet, Image} from 'react-native'
 
 interface Profile{
-    uuid: string,
+    NOME: string,
+    REGISTRO_ACADEMICO: string,
+    EMAIL: string,
+    FATEC_UNIDADE: string,
+    CURSO: string,
+    TURNO: string,
+    CICLO: number,
+    FOTO_URL: string
 }
 
 interface ProfileProps{
@@ -13,34 +20,67 @@ interface ProfileProps{
 const Profile: React.FC<ProfileProps> = ({uuid}) => {
     const backgroundColor = useThemeColor({ light: '#101010', dark: '#f0f0f0' }, 'background');
     const [profile, setProfile] = useState<Profile>();
-    const token = "d6ae5d9bcb7e4885517c3f60cf11da66";
+    const token = 'd6ae5d9bcb7e4885517c3f60cf11da66';
     
+    useEffect(()=>{
+        const buscarPerfil = async () => {
+            try{
+                const resposta = await axios(`http://192.168.1.6:80/api/aluno/all?uid=${uuid}`, {
+                  headers: {
+                    Authorization: `Bearer ${token}`
+                  }
+                });
 
+                const dados : Profile = resposta.data;
+                setProfile(dados);
+            }catch(error){
+              console.log(error)
+            }
+        }
+        buscarPerfil();
+    },[uuid])
   return (
-    <View style = {styles.container}>
-        
-        <Text style={styles.title}>Ola</Text>
-        <Image
-        source={require('@/assets/images/react-logo.png')}
-        style={styles.userImage}
-      />
-      
-        
 
-    </View>
+    <View style = {styles.container}>
+        <View style = {styles.header}>
+        <Image
+          source={{
+            uri: `${profile?.FOTO_URL}`
+          }}
+          style={styles.userImage}/>
+          <Text style={[styles.title, {color: backgroundColor}]}>{profile?.NOME}</Text>
+        </View>
+        <View style = {styles.item}>
+        <Text  style={[styles.title]}>{profile?.CURSO} - {profile?.CICLO}° Semestre ({profile?.TURNO})</Text>
+        <Text  style={[styles.campus]}>{profile?.FATEC_UNIDADE}</Text>
+        <Text  style={[styles.title]}>{}</Text>
+        <Text  style={[styles.title]}></Text>
+        </View>
+      </View>
+    
   )
 }
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      flexDirection: 'row',
       padding: 0,
-      alignItems: 'center'
     },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: "space-between",
+      marginEnd: 10
+    },
+
     title: {
       textAlign: 'center',
-      fontSize: 24,
-      fontWeight: 'bold',
+      fontSize: 15,
+      marginBottom: 20,
+      fontWeight: 'bold'
+    },
+    campus: {
+      textAlign: 'center',
+      fontSize: 15,
       marginBottom: 20,
     },
     item: {
@@ -54,33 +94,13 @@ const styles = StyleSheet.create({
     descricao: {
       fontWeight: 'bold',
     },
-    media: {
-      fontSize:15,
-      marginTop: 5,
-    },
-    mediaBold:{
-      fontSize:20,
-      fontWeight: 'bold'
-    },
-    prova: {
-      marginTop: 10,
-      padding: 10,
-      backgroundColor: '#e9e9e9',
-      borderRadius: 5,
-    },
-    provaId: {
-      fontWeight: 'bold',
-    },
-    provaData: {
-      marginTop: 5,
-    },
-    avaliacao: {
-      marginTop: 5,
-    },
+  
     userImage: {
-        height: 100,
-        width: 100,
-        
+        height: 90,
+        width: 90,
+        borderWidth: 1,
+        margin: 10,
+        borderColor: 'white', 
       },
   });
 
